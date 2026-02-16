5468a34ba8429dc9452b6e06232b0d81a43c2577
S.Hoomaan Tabatabaei <88086672+HoomaanTaba@users.noreply.github.com>
2026-02-16 14:20:11 +0330

    complete UI part of even code menu

--- Events.h
+++ Events.h
@@ -1,11 +1,9 @@
-//
-// Created by Ali on 26/01/2026.
-//
-
 #ifndef INC_14041016_EVENTS_H
 #define INC_14041016_EVENTS_H
 
 #include "UI_Objects.h"
+static int globalBlockID = 1000;
+// SideButton of events in code menu
 SideButton eventsBtn = {
         1,
         "Events",
@@ -13,5 +11,288 @@ SideButton eventsBtn = {
         {0,285,70,65},
         true
 };
+
+void initEvents(SDL_Renderer* renderer) {
+
+    // Hat Blocks
+    SDL_Texture* greenFlagTex = IMG_LoadTexture(renderer, "greenFlag.png");
+    SDL_Texture* keyPressTex  = IMG_LoadTexture(renderer, "keyPressed.png");
+    SDL_Texture* spriteClickTex  = IMG_LoadTexture(renderer, "spriteClick.png");
+    SDL_Texture* backdropTex  = IMG_LoadTexture(renderer, "backDrop.png");
+    SDL_Texture* loudnessTex  = IMG_LoadTexture(renderer, "loudness.png");
+    SDL_Texture* receiveTex  = IMG_LoadTexture(renderer, "receive.png");
+
+    // Command Blocks
+    SDL_Texture* broadcastTex  = IMG_LoadTexture(renderer, "broadcast.png");
+    SDL_Texture* broadcastWaitTex  = IMG_LoadTexture(renderer, "broadcastWait.png");
+
+    int startY = 100, gap = 70;
+
+    // when green flag
+    menuBlocks.push_back({
+        greenFlagTex,
+        {80, startY, 180, 60},
+        false, 0, 0,
+        true,
+        -1,
+        globalBlockID++
+    });
+
+    // when key press
+    DraggableBlock keyBlock = {
+            keyPressTex,
+            {80, startY+gap, 180, 60},
+            false, 0, 0,
+            true,
+            -1,
+            globalBlockID++
+    };
+    keyBlock.hasDropdown = true;
+    keyBlock.dropdownOptions = {"space", "up arrow", "down arow", "a", "b", "c"};
+    keyBlock.dropdownRect = {0,0,80,20};
+    menuBlocks.push_back(keyBlock);
+
+    // when sprite clicked
+    menuBlocks.push_back({
+        spriteClickTex,
+        {80, startY+gap*2, 180, 60},
+        false, 0, 0,
+        true,
+        -1,
+        globalBlockID++
+    });
+
+    // when backdrop switches (dropdown)
+    DraggableBlock backdropBlock = {
+            backdropTex,
+            {80, startY+gap*3, 180, 60},
+            false, 0, 0,
+            true,
+            globalBlockID++
+    };
+    backdropBlock.hasDropdown = true;
+    backdropBlock.dropdownOptions = {"backdrop1", "backdrop2"};
+    backdropBlock.dropdownRect = {0,0,80,20};
+    menuBlocks.push_back(backdropBlock);
+
+    // when loudness
+    menuBlocks.push_back({
+        loudnessTex,
+        {80, startY+gap*4, 180, 60},
+        false, 0,0,
+        true,
+        -1,
+        globalBlockID++
+    });
+
+    // when I receive
+    DraggableBlock receiveBlock = {
+            receiveTex,
+            {80, startY+gap*5, 180, 60},
+            false, 0,0,
+            true,
+            -1,
+            globalBlockID++
+    };
+    receiveBlock.hasDropdown = true;
+    receiveBlock.dropdownOptions = {"message1", "message2", "message3"};
+    receiveBlock.dropdownRect = {0,0,90,20};
+    menuBlocks.push_back(receiveBlock);
+
+    // broadcast
+    DraggableBlock broadcastBlock = {
+            broadcastTex,
+            {80, startY+gap*6, 180, 50},
+            false, 0, 0,
+            false,
+            -1,
+            globalBlockID++
+    };
+    broadcastBlock.hasDropdown = true;
+    broadcastBlock.dropdownOptions = {"message1", "message2", "message3"};
+    broadcastBlock.dropdownRect = {0,0,90,20};
+    menuBlocks.push_back(broadcastBlock);
+
+    // broadcast and wait
+    DraggableBlock broadcastWaitBlock = {
+            broadcastWaitTex,
+            {80, startY+gap*7, 180, 50},
+            false, 0,0,
+            false,
+            -1,
+            globalBlockID++
+    };
+    broadcastWaitBlock.hasDropdown = true;
+    broadcastWaitBlock.dropdownOptions = {"message1", "message2", "message3"};
+    broadcastWaitBlock.dropdownRect = {0,0,90,20};
+    menuBlocks.push_back(broadcastWaitBlock);
+}
+
+void handleEventBlock(SDL_Event& e, bool codeTabActive, bool eventBtnActive) {
+    int mx, my;
+    SDL_GetMouseState(&mx, &my);
+    if(e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT) {
+        bool caught = false;
+        for(int i = workspaceBlocks.size()-1; i >= 0; i--) {
+            if(isInside(mx, my, workspaceBlocks[i].rect)) {
+                workspaceBlocks[i].dragging = true;
+                workspaceBlocks[i].parentID = -1;
+                workspaceBlocks[i].offsetX = mx - workspaceBlocks[i].rect.x;
+                workspaceBlocks[i].offsetY = my - workspaceBlocks[i].rect.y;
+
+                DraggableBlock temp = workspaceBlocks[i];
+                workspaceBlocks.erase(workspaceBlocks.begin() + i);
+                workspaceBlocks.push_back(temp);
+                caught = true;
+                break;
+            }
+        }
+
+        if(!caught && codeTabActive && eventBtnActive) {
+            for(auto& mb:menuBlocks) {
+                if(isInside(mx, my, mb.rect)) {
+                    DraggableBlock newNode = mb;
+
+                    newNode.parentID = -1;
+                    newNode.dragging = true;
+
+                    newNode.rect.x = mx - mb.rect.w/2;
+                    newNode.rect.y = my - mb.rect.h/2;
+
+                    newNode.offsetX = mb.rect.w/2;
+                    newNode.offsetY = mb.rect.h/2;
+                    workspaceBlocks.push_back(newNode);
+                    break;
+                }
+            }
+        }
+    }
+
+    if(e.type == SDL_MOUSEBUTTONUP) {
+        for(auto& b : workspaceBlocks) {
+            if(b.dragging) {
+                b.dragging = false;
+                if(b.rect.x < 250) {
+                    for(int k = 0; k < workspaceBlocks.size(); k++) {
+                        if(&workspaceBlocks[k]==&b) {
+                            workspaceBlocks.erase(workspaceBlocks.begin() + k);
+                            break;
+                            return;
+                        }
+                    }
+                }
+                // Sticking part
+
+                for(auto& target : workspaceBlocks) {
+                    if(&b == &target)
+                        continue;
+
+                    int centerBX = b.rect.x + b.rect.w/2;
+                    int centerTX = target.rect.x + target.rect.w/2;
+
+                    int snapX = abs(centerBX - centerTX);
+                    int snapY = abs(b.rect.y - (target.rect.y + target.rect.h));
+
+                    if(snapX < 30 && snapY < 30) {
+                        if(b.isHat && target.parentID != -1)
+                            continue;
+                        b.rect.x = target.rect.x;
+                        b.rect.y = target.rect.y + target.rect.h-6; //Overlapping pixels
+                        b.parentID = target.id;
+                        break; // when fined good block, break;
+                    }
+                }
+            }
+        }
+    }
+    if(e.type == SDL_MOUSEMOTION) {
+        for(int i = 0; i < workspaceBlocks.size(); i++) {
+            if(workspaceBlocks[i].dragging) {
+                int oldX = workspaceBlocks[i].rect.x;
+                int oldY = workspaceBlocks[i].rect.y;
+
+                // Dragging the main block
+                workspaceBlocks[i].rect.x = mx - workspaceBlocks[i].offsetX;
+                workspaceBlocks[i].rect.y = my - workspaceBlocks[i].offsetY;
+
+                int diffX = workspaceBlocks[i].rect.x - oldX;
+                int diffY = workspaceBlocks[i].rect.y - oldY;
+
+                // Dragging all blocks under each other
+                bool changed = true;
+                vector<int> movedIndices;
+                movedIndices.push_back(i);
+                while(changed) {
+                    changed = false;
+                    for(int j = 0; j < workspaceBlocks.size(); j++) {
+                        bool alreadyMoved = false;
+                        for(int idx: movedIndices)
+                            if(idx==j)
+                                alreadyMoved = true;
+                        if(!alreadyMoved && j != i && !workspaceBlocks[j].dragging) {
+                            for(int idx: movedIndices) {
+                                // checking if idx is under a block
+                                if(workspaceBlocks[j].parentID == workspaceBlocks[idx].id && workspaceBlocks[j].parentID != -1) {
+                                    workspaceBlocks[j].rect.x += diffX;
+                                    workspaceBlocks[j].rect.y += diffY;
+                                    movedIndices.push_back(j);
+                                    changed = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void renderEventBlocks(SDL_Renderer* renderer,TTF_Font * font, bool codeTabActive, bool eventsBtnActive) {
+    if(codeTabActive && eventsBtnActive) {
+        for(auto& mb : menuBlocks) {
+            SDL_RenderCopy(renderer, mb.texture, NULL, &mb.rect);
+        }
+    }
+    for(auto& wb : workspaceBlocks) {
+        SDL_RenderCopy(renderer, wb.texture, NULL, &wb.rect);
+        if(wb.hasDropdown) {
+            wb.dropdownRect.x = wb.rect.x + wb.rect.w - 100;
+            wb.dropdownRect.y = wb.rect.y + 18;
+
+            SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+            SDL_RenderFillRect(renderer, &wb.dropdownRect);
+
+            SDL_SetRenderDrawColor(renderer, 150,150,150,255);
+            SDL_RenderDrawRect(renderer, &wb.dropdownRect);
+
+            SDL_Color textColor = {0,0,0,255};
+            drawTextCentered(renderer,font,
+                             wb.dropdownOptions[wb.selectedOption],
+                             wb.dropdownRect,
+                             textColor);
+
+            if(wb.dropdownOpen) {
+                for(int i = 0; i < wb.dropdownOptions.size(); i++) {
+                    SDL_Rect optionRect = {
+                            wb.dropdownRect.x,
+                            wb.dropdownRect.y + (i+1)*wb.dropdownRect.h,
+                            wb.dropdownRect.w,
+                            wb.dropdownRect.h
+                    };
+
+                    SDL_SetRenderDrawColor(renderer, 240,240,240,255);
+                    SDL_RenderFillRect(renderer, &optionRect);
+                    SDL_RenderDrawRect(renderer, &optionRect);
+
+                    drawTextCentered(renderer, font,
+                                     wb.dropdownOptions[i],
+                                     optionRect,
+                                     textColor);
+                }
+            }
+        }
+    }
+}
 
 #endif //INC_14041016_EVENTS_H
